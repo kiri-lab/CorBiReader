e5338a43b72c46e8af9ea847664a64cd89a4fb58
kirimine170 <kirimine170@gmail.com>
2024-01-31 14:32:33 +0900

    ✏️ update main.cpp
    プロジェクトの名前とか出すようにしておいた

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,118 @@
-#include <Arduino.h>
+#include <M5StickCPlus2.h>
 
-// put function declarations here:
-int myFunction(int, int);
+void morseLED(void *arg);
+void morseDot();
+void morseDash();
+void morseLetterPulse();
+void morseWordPulse();
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup()
+{
+  M5.begin();
+
+  Serial.begin(115200);
+  delay(500);
+
+  M5.Lcd.print("Farewell World\n");
+  M5.Lcd.print("It's CorBi\n");
+  M5.Lcd.print("made by kiri-lab\n");
+
+  xTaskCreatePinnedToCore(morseLED, "morseTask", 4096, NULL, 1, NULL, 1);
+  pinMode(19, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+int timeUnit = 50;
+void morseLED(void *arg)
+{
+  morseDash();
+  morseDash();
+  morseLetterPulse(); // m
+  morseDot();
+  morseDash();
+  morseLetterPulse(); // a
+  morseDash();
+  morseDot();
+  morseDot();
+  morseLetterPulse(); // d
+  morseDot();
+  // e
+  morseWordPulse(); // space
+  // by
+  morseDash();
+  morseDot();
+  morseDot();
+  morseDot();
+  morseLetterPulse(); // b
+  morseDash();
+  morseDot();
+  morseDash();
+  morseDash();
+  // y
+  morseWordPulse(); // space
+  // kiri
+  morseDash();
+  morseDot();
+  morseDash();
+  morseLetterPulse(); // k
+  morseDot();
+  morseDot();
+  morseLetterPulse(); // i
+  morseDot();
+  morseDash();
+  morseDot();
+  morseLetterPulse(); // r
+  morseDot();
+  morseDot();
+  morseLetterPulse(); // i
+  morseDash();
+  morseDot();
+  morseDot();
+  morseDot();
+  morseDot();
+  morseDash();
+  morseLetterPulse(); // -
+  morseDot();
+  morseDash();
+  morseDot();
+  morseDot();
+  morseLetterPulse(); // l
+  morseDot();
+  morseDash();
+  morseLetterPulse(); // a
+  morseDash();
+  morseDot();
+  morseDot();
+  morseDot();
+  // b
+  morseWordPulse(); // space
+}
+
+void morseDot()
+{
+  digitalWrite(19, HIGH);
+  delay(timeUnit);
+  digitalWrite(19, LOW);
+  delay(timeUnit);
+}
+
+void morseDash()
+{
+  digitalWrite(19, HIGH);
+  delay(timeUnit * 3);
+  digitalWrite(19, LOW);
+  delay(timeUnit);
+}
+
+void morseLetterPulse()
+{
+  delay(timeUnit * 3);
+}
+
+void morseWordPulse()
+{
+  delay(timeUnit * 7);
 }
