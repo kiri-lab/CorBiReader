bb0495d520861e622cdf918c588d23c91743209c
kirimine170 <kirimine170@gmail.com>
2024-01-31 20:08:33 +0900

    ✏️ update main.cpp
    心拍と酸素表示を追加

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,17 @@
 #include <M5StickCPlus2.h>
+#include <MAX30100_PulseOximeter.h>
+
+void pulseOximeter(void *arg);
 
 void morseLED(void *arg);
 void morseDot();
 void morseDash();
 void morseLetterPulse();
 void morseWordPulse();
+
+#define REPORTING_PERIOD_MS 1000
+PulseOximeter pox;
+uint32_t tsLastReport = 0;
 
 void setup()
 {
@@ -13,19 +20,48 @@ void setup()
   Serial.begin(115200);
   delay(500);
 
+  if (!pox.begin())
+  {
+    Serial.println("FAILED");
+  }
+  else
+  {
+    Serial.println("SUCCESS");
+  }
+
   M5.Lcd.setRotation(3);
   M5.Lcd.setTextFont(4);
   M5.Lcd.println("Farewell World");
   M5.Lcd.println("It's CorBi");
   M5.Lcd.print("made by ");
-  M5.Lcd.setTextColor(TFT_PURPLE);
+  M5.Lcd.setTextColor(TFT_PURPLE, TFT_BLACK);
   M5.Lcd.println("kiri-lab");
+  M5.Lcd.setTextColor(TFT_WHITE, TFT_BLACK);
+  M5.Lcd.println("HR");
+  M5.Lcd.println("O2");
 
-  xTaskCreatePinnedToCore(morseLED, "morseTask", 4096, NULL, 1, NULL, 1);
+  // xTaskCreatePinnedToCore(morseLED, "morseTask", 4096, NULL, 1, NULL, 1);
+  // xTaskCreatePinnedToCore(pulseOximeter, "MAX30100", 4096, NULL, 2, NULL, 1);
   pinMode(19, OUTPUT);
 }
 
 void loop()
+{
+  pox.update();
+  if (millis() - tsLastReport > REPORTING_PERIOD_MS)
+  {
+    M5.Lcd.setTextColor(TFT_WHITE, TFT_BLACK);
+    M5.Lcd.setCursor(40, 80);
+    M5.Lcd.print(pox.getHeartRate());
+
+    M5.Lcd.setCursor(40, 100);
+    M5.Lcd.print(pox.getSpO2());
+
+    tsLastReport = millis();
+  }
+}
+
+void pulseOximeter(void *arg)
 {
 }
 
