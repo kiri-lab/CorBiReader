ec825013cb5dc4bf2916fe0a7abe73ee777ce749
kirimine170 <kirimine170@gmail.com>
2024-02-04 04:00:43 +0900

    🚧 update main.cpp
    Advertisingと心拍の表示は同時にできた。

--- src/main.cpp
+++ src/main.cpp
@@ -21,25 +21,25 @@ void morseWordPulse();
 // Issue #10
 // 同居させた瞬間心拍が取得できなくなったので原因を探る
 
-// void startService(BLEServer *pServer);
-// void startAdvertising();
+void startService(BLEServer *pServer);
+void startAdvertising();
 
 PulseOximeter pox;
 uint32_t tsLastReport = 0;
-// BLECharacteristic *pCharaPOHR;
+BLECharacteristic *pCharaPOHR;
 
-// class CorBiServerCallbacks : public BLEServerCallbacks
-// {
-//   void onConnect(BLEServer *pServer)
-//   {
-//     M5.Lcd.println("Connected");
-//   }
+class CorBiServerCallbacks : public BLEServerCallbacks
+{
+  void onConnect(BLEServer *pServer)
+  {
+    M5.Lcd.println("Connected");
+  }
 
-//   void onDisconnect(BLEServer *pServer)
-//   {
-//     M5.Lcd.println("Disconnected");
-//   }
-// };
+  void onDisconnect(BLEServer *pServer)
+  {
+    M5.Lcd.println("Disconnected");
+  }
+};
 
 void setup()
 {
@@ -50,10 +50,10 @@ void setup()
 
   BLEDevice::init("CorBi"); // FIXME こいつ動かすと心拍が取得できなくなる
   // NOTE 最初に初期化すれば、poxは動いた
-  // BLEServer *pServer = BLEDevice::createServer();
-  //  pServer->setCallbacks(new CorBiServerCallbacks());
-  //  startService(pServer);
-  //  startAdvertising();
+  BLEServer *pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new CorBiServerCallbacks());
+  startService(pServer);
+  startAdvertising();
 
   M5.Lcd.setRotation(3);
   M5.Lcd.setTextFont(4);
@@ -93,8 +93,8 @@ void loop()
     M5.Lcd.setCursor(40, 100);
     M5.Lcd.print(pox.getSpO2());
 
-    // pCharaPOHR->setValue(hr);
-    // pCharaPOHR->notify();
+    pCharaPOHR->setValue(hr);
+    pCharaPOHR->notify();
 
     tsLastReport = millis();
   }
@@ -107,32 +107,32 @@ void pulseOximeter(void *arg)
 // FIXME いいからプロトタイピングだ！してるので、規格違反してたら後から直してください
 // 機能の実装を一旦優先します。
 
-// void startService(BLEServer *pServer)
-// {
-//   // 0x2800はプライマリサービス、0x00はインスタンスID
-//   // BLEService *pService = pServer->createService(BLEUUID(SERVICE_PULSEOXIMETER_UUID), (uint32_t)0x2800, (uint8_t)0x00); // FIXME ハードコーディングしてるけど、どうしよ
-//   BLEService *pService = pServer->createService(SERVICE_PULSEOXIMETER_UUID);
+void startService(BLEServer *pServer)
+{
+  // 0x2800はプライマリサービス、0x00はインスタンスID
+  // BLEService *pService = pServer->createService(BLEUUID(SERVICE_PULSEOXIMETER_UUID), (uint32_t)0x2800, (uint8_t)0x00); // FIXME ハードコーディングしてるけど、どうしよ
+  BLEService *pService = pServer->createService(SERVICE_PULSEOXIMETER_UUID);
 
-//   pCharaPOHR = pService->createCharacteristic(
-//       CHARA_PO_HR_UUID,
-//       BLECharacteristic::PROPERTY_READ |
-//           BLECharacteristic::PROPERTY_WRITE);
-//   BLEDescriptor *pDescriptor = new BLEDescriptor(BLEUUID((uint16_t)0x0963)); // FIXME ハードコーディング
-//   // pDescriptor->setValue((uint8_t *)64, 1); // FIXME 使い方いまいち分からん
-//   pDescriptor->setValue("Heart Rate"); // Stringで指定ならいける
-//   pCharaPOHR->addDescriptor(pDescriptor);
+  pCharaPOHR = pService->createCharacteristic(
+      CHARA_PO_HR_UUID,
+      BLECharacteristic::PROPERTY_READ |
+          BLECharacteristic::PROPERTY_WRITE);
+  BLEDescriptor *pDescriptor = new BLEDescriptor(BLEUUID((uint16_t)0x0963)); // FIXME ハードコーディング
+  // pDescriptor->setValue((uint8_t *)64, 1); // FIXME 使い方いまいち分からん
+  pDescriptor->setValue("Heart Rate"); // Stringで指定ならいける
+  pCharaPOHR->addDescriptor(pDescriptor);
 
-//   pCharaPOHR->setValue("963");
-//   pService->start();
-// }
+  pCharaPOHR->setValue("963");
+  pService->start();
+}
 
-// void startAdvertising()
-// {
-//   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
-//   pAdvertising->addServiceUUID(SERVICE_PULSEOXIMETER_UUID);
-//   pAdvertising->setScanResponse(true);
-//   BLEDevice::startAdvertising();
-// }
+void startAdvertising()
+{
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_PULSEOXIMETER_UUID);
+  pAdvertising->setScanResponse(true);
+  BLEDevice::startAdvertising();
+}
 
 int timeUnit = 50;
 void morseLED(void *arg)
